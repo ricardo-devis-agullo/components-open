ac84c8a997cf0bb8b954d8c5b9246ec5defab7e0
Ricardo Devis Agullo <mkredz@gmail.com>
2022-11-29 20:00:21 +0100

    fix init template for windows

--- src/api/domain/init_template.ts
+++ src/api/domain/init_template.ts
@@ -1,11 +1,11 @@
-import { join, copy } from '../../deps.ts';
+import { join, copy, fromFileUrl } from '../../deps.ts';
 
 export default async function initTemplate({
   componentPath,
 }: {
   componentPath: string;
 }): Promise<void> {
-  const __dirname = new URL('.', import.meta.url).pathname;
+  const __dirname = fromFileUrl(new URL('.', import.meta.url));
   const scaffoldPath = join(__dirname, 'scaffold');
 
   await copy(scaffoldPath, componentPath);
