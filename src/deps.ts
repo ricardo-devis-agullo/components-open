2b693023e579232c51512f74a8b7b6b28d89de12
Ricardo Devis Agullo <mkredz@gmail.com>
2023-03-10 23:21:46 +0100

    update deps

--- src/deps.ts
+++ src/deps.ts
@@ -5,19 +5,19 @@ export {
   resolve,
   toFileUrl,
   fromFileUrl,
-} from 'https://deno.land/std@0.165.0/path/mod.ts';
-export * as colors from 'https://deno.land/std@0.165.0/fmt/colors.ts';
-export { emptyDir } from 'https://deno.land/std@0.165.0/fs/empty_dir.ts';
-export { copy } from 'https://deno.land/std@0.165.0/fs/copy.ts';
-export { walk } from 'https://deno.land/std@0.165.0/fs/walk.ts';
-export { ensureDir } from 'https://deno.land/std@0.165.0/fs/ensure_dir.ts';
-export * as semver from 'https://deno.land/std@0.165.0/semver/mod.ts';
-export { delay } from 'https://deno.land/std@0.166.0/async/delay.ts';
+} from 'https://deno.land/std@0.179.0/path/mod.ts';
+export * as colors from 'https://deno.land/std@0.179.0/fmt/colors.ts';
+export { emptyDir } from 'https://deno.land/std@0.179.0/fs/empty_dir.ts';
+export { copy } from 'https://deno.land/std@0.179.0/fs/copy.ts';
+export { walk } from 'https://deno.land/std@0.179.0/fs/walk.ts';
+export { ensureDir } from 'https://deno.land/std@0.179.0/fs/ensure_dir.ts';
+export * as semver from 'https://deno.land/std@0.179.0/semver/mod.ts';
+export { delay } from 'https://deno.land/std@0.179.0/async/delay.ts';
 
-export * as checksum from 'https://deno.land/x/checksum@1.2.0/mod.ts';
-export * as oak from 'https://deno.land/x/oak@v11.1.0/mod.ts';
-export { Command } from 'https://deno.land/x/cliffy@v0.25.4/command/mod.ts';
-export { AzureStorage as AzureStorageClient } from 'https://deno.land/x/azure_storage_client@0.5.0/mod.ts';
-export * as esbuild from 'https://deno.land/x/esbuild@v0.15.15/mod.js';
+export * as checksum from 'https://deno.land/x/checksum@1.4.0/mod.ts';
+export * as oak from 'https://deno.land/x/oak@v12.1.0/mod.ts';
+export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
+export { AzureStorage as AzureStorageClient } from 'https://deno.land/x/azure_storage_client@0.7.0/mod.ts';
+export * as esbuild from 'https://deno.land/x/esbuild@v0.17.11/mod.js';
 export { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts';
-export { parse as XMLParse } from 'https://deno.land/x/xml@2.0.4/mod.ts';
+export { parse as XMLParse } from 'https://deno.land/x/xml@2.1.0/mod.ts';
